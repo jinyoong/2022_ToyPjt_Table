6c8bdad45caff456994c52a8a1bf7afb27149f29
jinyong <zhaqh146@naver.com>
2022-11-14 14:41:42 +0900

    feat: 드롭다운, 페이지네이션 기능 구현

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function title() {
+  const titleArea = document.createElement('div');
+
   const title = document.createElement('h2');
   title.textContent = '2022 토이 프로젝트: 테이블 구현하기';
-  return title;
+
+  titleArea.appendChild(title);
+  return titleArea;
 };
 
 async function getData() {
@@ -9,7 +13,10 @@ async function getData() {
   return response.json();
 };
 
-function makeTable(datas) {
+function makeTable(datas, start, end) {
+  const tableArea = document.createElement('div');
+  tableArea.id = 'table';
+
   const table = document.createElement('table');
   
   const tableHead = document.createElement('thead');
@@ -26,7 +33,7 @@ function makeTable(datas) {
 
   const tableBody = document.createElement('tbody');
   
-  datas.forEach(data => {
+  datas.slice(start, end).forEach(data => {
     const tableBodyLine = document.createElement('tr');
     
     Object.values(data).forEach(value => {
@@ -40,11 +47,94 @@ function makeTable(datas) {
 
   table.appendChild(tableBody);
 
-  return table;
+  tableArea.appendChild(table);
+  return tableArea;
 };
 
+function removeTable() {
+  const table = document.getElementById('table');
+
+  if (table) {
+    table.remove();
+  };
+}
+
+function dropDown(datas) {
+  const dropDownArea = document.createElement('div');
+
+  const dropDown = document.createElement('select');
+  const options = [
+    {value: 4, label: '4개씩'},
+    {value: 8, label: '8개씩'},
+  ];
+
+  options.forEach(element => {
+    const option = document.createElement('option');
+    option.value = element.value;
+    option.textContent = element.label;
+    dropDown.appendChild(option);
+  })
+
+  dropDown.addEventListener('change', event => {
+    removeTable();
+    removePagiNation();
+    root.appendChild(makeTable(datas, 0, event.target.value));
+    root.appendChild(pagiNation(datas, event.target.value));
+  });
+
+  dropDownArea.appendChild(dropDown);
+  return dropDownArea;
+};
+
+
+function pagiNation(datas, per) {
+  const pageCount = Math.ceil(datas.length / per);
+  const pagiNation = document.createElement('div');
+  pagiNation.id = 'pagination';
+
+  const page = document.createElement('div');
+  
+  const moveLeft = document.createElement('button');
+  moveLeft.textContent = '<<';
+  moveLeft.addEventListener('click', () => {
+    removeTable();
+    root.appendChild(makeTable(datas, 0, per));
+  });
+  page.appendChild(moveLeft);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const pageElement = document.createElement('button');
+    pageElement.textContent = i;
+    pageElement.addEventListener('click', () => {
+      removeTable();
+      root.appendChild(makeTable(datas, per * (i - 1), per * i));
+    });
+    page.appendChild(pageElement);
+  };
+
+  const moveRight = document.createElement('button');
+  moveRight.textContent = '>>';
+  moveRight.addEventListener('click', () => {
+    removeTable();
+    root.appendChild(makeTable(datas, per * (pageCount - 1), datas.length));
+  });
+  page.appendChild(moveRight);
+
+  pagiNation.appendChild(page);
+  return pagiNation;
+}
+
+function removePagiNation() {
+  const pagiNation = document.getElementById('pagination');
+
+  if (pagiNation) {
+    pagiNation.remove();
+  };
+}
 
 const tableData = await getData();
 const root = document.getElementById('root');
 root.appendChild(title());
-root.appendChild(makeTable(tableData));
+root.appendChild(dropDown(tableData));
+root.appendChild(makeTable(tableData, 0, 4));
+root.appendChild(pagiNation(tableData, 4));
