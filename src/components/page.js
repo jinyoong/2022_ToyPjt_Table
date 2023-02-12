15f6b0bb72edcb8e93bbb5e24ddf99a6096b9092
jinyong <zhaqh146@naver.com>
2023-02-12 19:43:20 +0900

    removePagination 분리 및 changePage 함수명 pageButton 으로 변경

---
+++ src/components/page.js
@@ -0,0 +1,41 @@
+import { changePage } from "../../index.js";
+
+function pagination(data, per, currentPage) {
+  const root = document.getElementById('root');
+  const pageCount = Math.ceil(data.length / per);
+  const pagiNation = document.createElement('div');
+  pagiNation.id = 'pagination';
+
+  const leftButton = document.createElement('button');
+  leftButton.className = 'pageButton';
+  leftButton.textContent = '<<';
+  pagiNation.appendChild(leftButton);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const pageButton = document.createElement('button');
+    pageButton.className = 'pageButton';
+    pageButton.textContent = i;
+    pagiNation.appendChild(pageButton);
+  };
+
+  const rightButton = document.createElement('button');
+  rightButton.className = 'pageButton';
+  rightButton.textContent = '>>';
+  pagiNation.appendChild(rightButton);
+  
+  pagiNation.addEventListener('click', pageButton);
+
+  root.appendChild(pagiNation);
+};
+
+function pageButton(event) {
+  changePage(event.target.textContent);
+};
+
+function removePagination() {
+  const pagiNation = document.getElementById('pagination');
+
+  if (pagiNation) pagiNation.remove();
+};
+
+export { pagination, changePage, removePagination };
